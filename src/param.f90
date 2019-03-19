9cedfc05f36afc63dfa5db0097ca0c6dcb3aaf3e
hbulou <herve.bulou@ipcms.unistra.fr>
2019-03-19 21:21:17 +0100

    * changing the way to deal with the input parameter file

--- src/param.f90
+++ src/param.f90
@@ -49,139 +49,150 @@ contains
   end subroutine init_param
   ! --------------------------------------------------------------------------------------
   !
+  !              parse_line()
+  !
+  ! --------------------------------------------------------------------------------------
+  subroutine parse_line(param,field,end_loop)
+    implicit none
+    type(t_param)::param
+    character (len=32)::field(32)
+    logical::end_loop
+    
+    integer::i
+    if(field(1).eq."box_radius >") then
+       read(field(2),*) param%box%radius
+    end if
+    if(field(1).eq."box_width >") then
+       read(field(2),*) param%box%width
+    end if
+    if(field(1).eq."box_shape >") then
+       read(field(2),*) param%box%shape
+    end if
+    if(field(1).eq."box_center >") then
+       read(field(2),*) param%box%center(1)
+       read(field(3),*) param%box%center(2)
+       read(field(4),*) param%box%center(3)
+    end if
+    if(field(1).eq."cmd >") then
+       if(field(2).eq."end") then
+          end_loop=.TRUE.
+       end if
+    end if
+    if(field(1).eq."dimension >") then
+       read(field(2),*) param%dim
+    end if
+    if(field(1).eq."ETA >") then
+       read(field(2),*) param%ETA
+    end if
+    if(field(1).eq."exchange >") then
+       read(field(2),*) param%exchange
+    end if
+    if(field(1).eq."extrap_add >") then
+       read(field(2),*) param%extrap_add
+    end if
+    if(field(1).eq."extrapol >") then
+       read(field(2),*) param%extrapol
+    end if
+    if(field(1).eq."hartree >") then
+       read(field(2),*) param%hartree
+    end if
+    if(field(1).eq."init_wf >") then
+       read(field(2),*) param%init_wf
+    end if
+    if(field(1).eq."perturb_intensity >") then
+       read(field(2),*) param%perturb%Intensity
+    end if
+    if(field(1).eq."perturb_sigma >") then
+       read(field(2),*) param%perturb%sigma
+    end if
+    if(field(1).eq."perturb_shape >") then
+       read(field(2),*) param%perturb%shape
+    end if
+    if(field(1).eq."perturb_location >") then
+       read(field(2),*) param%perturb%location(1)
+       read(field(3),*) param%perturb%location(2)
+       read(field(4),*) param%perturb%location(3)
+    end if
+    
+    
+    
+    if(field(1).eq."loopmax >") then
+       read(field(2),*) param%loopmax
+    end if
+    if(field(1).eq."noccstate >") then
+       read(field(2),*) param%noccstate
+    end if
+    if(field(1).eq."nvecmax >") then
+       read(field(2),*) param%nvecmax
+    end if
+    if(field(1).eq."nvecmin >") then
+       read(field(2),*) param%nvecmin
+    end if
+    if(field(1).eq."nvec_to_cvg >") then
+       read(field(2),*) param%nvec_to_cvg
+       deallocate(param%list_idx_to_cvg)
+       allocate(param%list_idx_to_cvg(param%nvec_to_cvg))
+       do i=1,param%nvec_to_cvg
+          param%list_idx_to_cvg(i)=i
+       end do
+       deallocate(param%occupation)
+       allocate(param%occupation(param%nvec_to_cvg))
+       do i=1,param%nvec_to_cvg
+          param%occupation(i)=1.0
+       end do
+    end if
+    if(field(1).eq."Nx >") then
+       read(field(2),*) param%Nx
+    end if
+    if(field(1).eq."occupation >") then
+       do i=1,param%nvec_to_cvg
+          read(field(1+i),*) param%occupation(i)
+       end do
+    end if
+    if(field(1).eq."prefix >") then
+       read(field(2),*) param%prefix
+    end if
+    if(field(1).eq."restart >") then
+       read(field(2),*) param%restart
+    end if
+    if(field(1).eq."scheme >") then
+       read(field(2),*) param%scheme
+    end if
+    if(field(1).eq."Zato >") then
+       read(field(2),*) param%Z
+    end if
+    if(field(1).eq."lorb >") then
+       read(field(2),*) param%lorb
+    end if
+  end subroutine parse_line
+  ! --------------------------------------------------------------------------------------
+  !
   !              read_param()
   !
   ! --------------------------------------------------------------------------------------
   subroutine read_param(param)
     implicit none
+    type(t_param)::param
+
     character (len=1024)::line,redline
     character (len=1024)::line2
     character (len=32)::field(32)
     integer::nfield,debidx,endidx
     logical::exist_file
-    type(t_param)::param
     integer::lline,eqidx,i,di
-    logical::eol
+    logical::eol,end_loop
 
     call init_param(param)
 
 
     print *,'Reading ',param%inputfile
     open(unit=1,file=param%inputfile,form='formatted')
-
-    do while(.not.(is_iostat_end(param%ieof)))
+    end_loop=.FALSE.
+    do while((.not.(is_iostat_end(param%ieof))).and.(.not.(end_loop)))
        read(1,'(A)') line
        call line_parser(line,nfield,field)
        print *,nfield,' --> ',(trim(field(i)),i=1,nfield)
-
-       if(field(1).eq."box_radius >") then
-          read(field(2),*) param%box%radius
-       end if
-       if(field(1).eq."box_width >") then
-          read(field(2),*) param%box%width
-       end if
-       if(field(1).eq."box_shape >") then
-          read(field(2),*) param%box%shape
-       end if
-       if(field(1).eq."box_center >") then
-          read(field(2),*) param%box%center(1)
-          read(field(3),*) param%box%center(2)
-          read(field(4),*) param%box%center(3)
-       end if
-       if(field(1).eq."cmd >") then
-          if(field(2).eq."end") then
-             exit
-          end if
-       end if
-       if(field(1).eq."dimension >") then
-          read(field(2),*) param%dim
-       end if
-       if(field(1).eq."ETA >") then
-          read(field(2),*) param%ETA
-       end if
-       if(field(1).eq."exchange >") then
-          read(field(2),*) param%exchange
-       end if
-       if(field(1).eq."extrap_add >") then
-          read(field(2),*) param%extrap_add
-       end if
-       if(field(1).eq."extrapol >") then
-          read(field(2),*) param%extrapol
-       end if
-       if(field(1).eq."hartree >") then
-          read(field(2),*) param%hartree
-       end if
-       if(field(1).eq."init_wf >") then
-          read(field(2),*) param%init_wf
-       end if
-       if(field(1).eq."perturb_intensity >") then
-          read(field(2),*) param%perturb%Intensity
-       end if
-       if(field(1).eq."perturb_sigma >") then
-          read(field(2),*) param%perturb%sigma
-       end if
-       if(field(1).eq."perturb_shape >") then
-          read(field(2),*) param%perturb%shape
-       end if
-       if(field(1).eq."perturb_location >") then
-          read(field(2),*) param%perturb%location(1)
-          read(field(3),*) param%perturb%location(2)
-          read(field(4),*) param%perturb%location(3)
-       end if
-
-
-
-       if(field(1).eq."loopmax >") then
-          read(field(2),*) param%loopmax
-       end if
-       if(field(1).eq."noccstate >") then
-          read(field(2),*) param%noccstate
-       end if
-       if(field(1).eq."nvecmax >") then
-          read(field(2),*) param%nvecmax
-       end if
-       if(field(1).eq."nvecmin >") then
-          read(field(2),*) param%nvecmin
-       end if
-       if(field(1).eq."nvec_to_cvg >") then
-          read(field(2),*) param%nvec_to_cvg
-          deallocate(param%list_idx_to_cvg)
-          allocate(param%list_idx_to_cvg(param%nvec_to_cvg))
-          do i=1,param%nvec_to_cvg
-             param%list_idx_to_cvg(i)=i
-          end do
-          deallocate(param%occupation)
-          allocate(param%occupation(param%nvec_to_cvg))
-          do i=1,param%nvec_to_cvg
-             param%occupation(i)=1.0
-          end do
-       end if
-       if(field(1).eq."Nx >") then
-          read(field(2),*) param%Nx
-       end if
-       if(field(1).eq."occupation >") then
-          do i=1,param%nvec_to_cvg
-             read(field(1+i),*) param%occupation(i)
-          end do
-       end if
-       if(field(1).eq."prefix >") then
-          read(field(2),*) param%prefix
-       end if
-       if(field(1).eq."restart >") then
-          read(field(2),*) param%restart
-       end if
-       if(field(1).eq."scheme >") then
-          read(field(2),*) param%scheme
-       end if
-       if(field(1).eq."Zato >") then
-          read(field(2),*) param%Z
-       end if
-       if(field(1).eq."lorb >") then
-          read(field(2),*) param%lorb
-       end if
-
-
-       
+       call parse_line(param,field,end_loop)
     end do
     close(1)    
 
